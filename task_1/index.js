062ded345b3d372e946fed8d15cd1b950f409af1
Smirnov Ivan <mr.smirnov.ia@gmail.com>
2024-04-30 12:51:12 +0300

    Task 1: decoding fields with the id suffix

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,29 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
 
+const excludeKeys = ['groupId'];
 
+function decode(encodeArr, excludeKeysArr = excludeKeys, translationsObj = translations) {
+	const decoded = encodeArr.map(obj => {
+		const decodedItem = {};
 
-// console.log(decoded)
+		Object.entries(obj).forEach(([key, value]) => {
+			if (!excludeKeysArr.includes(key) && key.endsWith('Id') && Object.hasOwn(translationsObj, value)) {
+				decodedItem[key] = translationsObj[value];
+			}
+			else {
+				decodedItem[key] = value;
+			}
+		})
+
+		return decodedItem;
+	})
+	return decoded;
+}
+
+const decoded = decode(encoded);
+
+console.log('encoded:', encoded);
+console.log('translations:', translations);
+console.log('decoded:', decoded);
